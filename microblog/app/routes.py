7bf482bf9bb53825087c1dc4051b7c6dc5f105a0
Winston Wu <161237354+winstonxwu@users.noreply.github.com>
2025-06-06 22:10:52 -0400

    Updatted code to latest version

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app # routes handles the different URLS for the application. 
+
 
 @app.route('/') # decorators, basically when the URLs / and /index are requested by the browser, Flask returns this function
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
+    user = {'Username': 'Nestor'}
+    posts = [
+        {
+            'author': {'username': 'Winston'},
+            'body': 'Beautiful day in the town!'
+        },
+        {
+            'author': {'username': 'Winston\'s pessimistic friend'},
+            'body': 'I did not care for the Godfather.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
