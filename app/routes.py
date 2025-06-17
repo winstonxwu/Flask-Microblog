940025e7ca017600211ee7364574fed2314fd52f
Winston Wu <161237354+winstonxwu@users.noreply.github.com>
2025-06-16 23:12:07 -0400

    username dynamic route to set username to browser request username

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,36 @@
-from flask import render_template
-from app import app # routes handles the different URLS for the application. 
-from app.forms import LoginForm
+from flask import render_template, redirect, flash, url_for, request
+from app import app, db # routes handles the different URLS for the application. 
+from app.forms import LoginForm, RegistrationForm
+import sqlalchemy as sa
+from app.models import User
+from flask_login import current_user, login_user, logout_user, login_required
+from urllib.parse import urlsplit
 
 
 @app.route('/') # decorators, basically when the URLs / and /index are requested by the browser, Flask returns this function
 @app.route('/index')
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+@app.route('/login', methods=['GET', 'POST'])
+@app.route('/logout')
+@app.route('/register', methods=['GET', 'POST'])
+@login_required
 
+
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data): # catch if the user is not valid
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '': # catch if user tries to access without being logged in
+            next_page = url_for('index')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 def index():
     user = {'Username': 'Nestor'}
     posts = [
@@ -22,4 +43,26 @@ def index():
             'body': 'I did not care for the Godfather.'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
